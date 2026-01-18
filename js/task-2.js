9521fb29e8e7af3063bb1c82c070041774ef9f86
nikuliia <iuliianiko@gmail.com>
2026-01-18 18:05:34 +0100

    commit

--- js/task-2.js
+++ js/task-2.js
@@ -14,9 +14,3 @@ console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); //
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-
-// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray (масив), secondArray (масив) і maxLength (число).
-// Функція повинна створювати новий масив, який містить усі елементи з firstArray, а потім усі елементи з secondArray.
-
-// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів.
-// В іншому випадку функція повинна повернути весь новий масив.
